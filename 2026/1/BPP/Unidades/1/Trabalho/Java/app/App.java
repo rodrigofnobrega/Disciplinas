de5b0ace6a990caf11215825f1fa8c0b41d414fe
Rodrigo <rodrigofnobrega1@gmail.com>
2026-05-10 18:45:28 -0300

    fix: Corrigir injeção de dependência

--- 2026/1/BPP/Unidades/1/Trabalho/Java/app/App.java
+++ 2026/1/BPP/Unidades/1/Trabalho/Java/app/App.java
@@ -2,13 +2,16 @@ package app;
 
 import app.menu.IMenuSistema;
 import app.menu.MenuSistema;
+import app.noticia.AnalisadorNoticia;
+import app.noticia.IAnalisadorNoticia;
 
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Sistema sistema = new Sistema();
+        IAnalisadorNoticia analisadorNoticia = new AnalisadorNoticia();
+        Sistema sistema = new Sistema(analisadorNoticia);
 
         IMenuSistema menuSistema = new MenuSistema(sistema, scanner);
 
